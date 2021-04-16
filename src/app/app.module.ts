0fed382af30208465992ab6104c02c3e4f59d985
Matheus Vieira <mvdias20@gmail.com>
2021-04-16 11:02:08 -0300

    Project Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full',
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/courses/course.module.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { StarModule } from "../shared/component/star/star.module";
+import { CourseInfoComponent } from "./course-info.component";
+import { CourseListComponent } from "./course-list.component";
+
+@NgModule ({
+    declarations: [
+        CourseListComponent,
+        CourseInfoComponent
+    ], imports: [
+        CommonModule,
+        FormsModule,
+        StarModule,
+        RouterModule.forChild([
+            {
+                path: 'courses', component: CourseListComponent,
+              },
+              {
+                path: 'courses/info/:id', component: CourseInfoComponent,
+              },
+        ])
+    ]
+})
+
+export class CourseModule {
+
+}
